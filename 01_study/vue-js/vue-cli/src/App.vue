96f65bdc95b5f58f68ac5d749d3a5903309b87fe
minsun <judy.leems@gmail.com>
2020-01-06 01:47:28 +0900

    vue cli 작업 중

---
+++ 01_study/vue-js/vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ str }}
+  </div>
+</template>
+
+<script>
+
+
+// new Vue({
+//   data: {
+//     srt: 'hi'
+//   }
+// })
+// vue cli 를 사용하면 위에 처럼 사용 하게 되면 에러남 
+// vue cli를 사용하는것 자체가 컴포넌트화? 해서 사용할 확률이 높기 때문에 각각 함수에 담아서 사용해야한다.
+
+export default {
+  data: function() {
+    return {
+      str: 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
